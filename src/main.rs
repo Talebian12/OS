6a3d592b5dffc8f3fe3746194773944701ca7c34
Talebian12 <talebian@sovietunion.xyz>
2020-12-21 01:16:26 +0100

    Finalized heap allocation changed: Cargo.toml changed: src/allocator.rs changed: src/main.rs new file: tests/heap_allocation.rs

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,12 @@ extern crate alloc;
 use x86_64::structures::paging::PageTable;
 use bootloader::{ BootInfo, entry_point};
 use core::panic::PanicInfo;
-use alloc::boxed::Box;
+use alloc::{
+    boxed::Box,
+    vec,
+    vec::Vec,
+    rc::Rc
+};
 use os::println;
 
 /// SET ENTRYPOINT
@@ -25,6 +30,7 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! 
 {
     use os::memory;
+    use os::allocator;
     use os::memory::BootInfoFrameAllocator;
     use x86_64::{
         structures::paging::Page,
@@ -51,8 +57,29 @@ fn kernel_main(boot_info: &'static BootInfo) -> !
         page_ptr.offset(400)
                 .write_volatile(0x_f021_f077_f065_f04e)
     };
-    
-    let x = Box::new(41);
+   
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    /// TEST HEAP 
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     // START TEST <- CARGO TEST
     #[cfg(test)]
