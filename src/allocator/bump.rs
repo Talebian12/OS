4f3282ca5932559697343dc3241be454563bcb70
Talebian12 <talebian@sovietunion.xyz>
2020-12-25 21:01:49 +0100

    Implemented GlobalAlloc for Bump Allocator changed: src/allocator/bump.rs

--- src/allocator/bump.rs
+++ src/allocator/bump.rs
@@ -1,6 +1,9 @@
 // src/allocator/bump.rs
 
-
+use alloc::alloc::{
+    GlobalAlloc, 
+    Layout,
+};
 
 pub struct BumpAllocator {
     heap_start:  usize,
@@ -19,4 +22,28 @@ impl BumpAllocator {
             next:        0,
         }
     }
+
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.heap_start = heap_start;
+        self.heap_end = heap_start + heap_size;
+        self.next = heap_start;
+    }
+}
+
+unsafe impl GlobalAlloc for BumpAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        //TODO: alignment and bound check
+        
+        let alloc_start = self.next;
+        
+        self.next = alloc_start + layout.size();
+        self.allocations += 1;
+        
+        alloc_start as *mut u8
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        todo!()
+    }
+
 }
