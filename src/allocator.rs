b4dca26be772cdb2124e6235a0041e6ed573fb26
Talebian12 <talebian@sovietunion.xyz>
2020-12-25 22:26:12 +0100

    Implemented Bump Allocator with Mutex changed: src/allocator.rs changed: src/allocator/bump.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -43,6 +43,23 @@ unsafe impl GlobalAlloc for Dummy {
     }
 }
 
+/// WRAPPER AROUND MUTEX TO PERMIT TRAIT IMPL
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
 
 /// INIT
 
